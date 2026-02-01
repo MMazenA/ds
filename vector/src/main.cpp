f8e6576a0546614b270ed072542366672a555b26
Mazen Mirza <84817881+MMazenA@users.noreply.github.com>
2026-01-31 22:40:55 -0500

    Iterator header (#1)
    * slice start end works
    
    * working stepping?
    
    * whatever this is

--- vector/src/main.cpp
+++ vector/src/main.cpp
@@ -8,6 +8,8 @@
 #include <string>
 #include <thread>
 
+using std::println;
+
 struct LargeStruct {
   LargeStruct(std::size_t s = 65536) : size(s), data(new double[s]) {
     for (std::size_t i = 0; i < size; ++i) {
@@ -65,8 +67,7 @@ void test_move(const int &iterations, bool move) {
   }
   auto end = std::chrono::high_resolution_clock::now();
   auto duration = duration_cast<std::chrono::milliseconds>(end - start).count();
-  std::println("Time {1}: {0} ms", duration,
-               move ? "with move" : "without move");
+  println("Time {1}: {0} ms", duration, move ? "with move" : "without move");
 }
 
 int main() {
@@ -75,23 +76,27 @@ int main() {
   test_move(iterations, true);
   ds::vector<int> vec;
   vec.push_back(5);
+  vec.push_back(15);
+  vec.push_back(20);
+  vec.push_back(25);
 
+  std::print("[");
   for (auto x : vec) {
-    if (x == 5) {
-      vec.push_back(20);
-      vec.push_back(20);
-      vec.push_back(20);
-    }
-    std::println("{0}", x);
+    std::print("{0} ", x);
   }
+  println("]");
+
+  println("");
+  for (auto x : vec.slice(2, ds::end, 2)) {
+    println("{0}", x);
+  }
+
   vec.push_back(3);
-  std::println("reverse indexing: {0}", vec(-1));
 
-  for (auto x : vec.what()) {
+  println("");
+  println("");
+  println("reverse indexing: {0}", vec(-1));
 
-    std::println("What? : {}", x);
-  }
-
-  std::println("Passed");
+  println("Passed");
   return 0;
 }
