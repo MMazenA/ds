5a4bad10b9df6de225fee85f834da591d6ebcbb9
MMazenA <jazenjirza@gmail.com>
2026-06-07 14:45:56 -0400

    fix gtest build

--- triplets/clang-libcxx-toolchain.cmake
+++ triplets/clang-libcxx-toolchain.cmake
@@ -1,2 +1,6 @@
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-stdlib=libc++")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-stdlib=libc++")
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
